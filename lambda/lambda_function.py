dd9e9420ea664b57e16851ceadfde541fa989814
aki-nishikawa <akihito.n.0910@keio.jp>
2022-06-21 00:22:21 +0900

    get slack conversation history by using slack API

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,41 +1,78 @@
+# import boto3
 import json
-import boto3
+import logging
 
-comprehend = boto3.client('comprehend')
+import requests
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# slack API client
+USER_TOKEN = 'xoxp-3520750066675-3514126860774-3577573220384-e8db3feed159abbd81e967571ffa9e28'
+BOT_TOKEN = 'xoxb-3520750066675-3556193162788-Nr7idPBHOrMmqR12ljcT8bME'
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=BOT_TOKEN)
+logger = logging.getLogger(__name__)
+
+# comprehend client
+# comprehend = boto3.client('comprehend')
 
 def lambda_handler(event, context):
-    input_text = "とても美味しいです！"
-    #comprehend接続のテスト 
+
+    channel_name = "授業"
+
+    ### get slack log
+    # get channel id
+    channel_id = None
+    try:
+        # Call the conversations.list method using the WebClient
+        for result in client.conversations_list():
+            if channel_id is not None:
+                break
+            for channel in result["channels"]:
+                if channel["name"] == channel_name:
+                    channel_id = channel["id"]
+                    #Print result
+                    print(f"Found conversation ID: {channel_id}")
+                    break
+    except SlackApiError as e:
+        print(f"Error: {e}")
+        # TODO: error handling
+    
+    if channel_id is None:
+        print(f"Error: No such channel: {channel_name}")
+        # TODO: error handling
+        return
+    
+    # Store conversation history
+    conversation_history = []
+    try:
+        # Call the conversations.history method using the WebClient
+        # conversations.history returns the first 100 messages by default
+        # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+        conversation_history = client.conversations_history(channel=channel_id)["messages"]
+        # logger.info("{} messages found in {}".format(len(conversation_history), id))
+
+    except SlackApiError as e:
+        logger.error("Error creating conversation: {}".format(e))
+    
+    return conversation_history
+
+    ### comprehend接続のテスト 
+    # input_text = "とても美味しいです！"
     # response = comprehend.detect_sentiment(
     #     Text='input_text',
     #     LanguageCode='ja'
     # )    
     # sentiment_score = response.get('SentimentScore')
-
     # return {
     #     'statusCode': 200,
     #     'body': json.dumps({
     #         'sentiment_score': sentiment_score
     #     })
     # }
-    #
-    url = "https://slack.com/api/conversations.history" 
-    token = "xoxb-3520750066675-3556193162788-Nr7idPBHOrMmqR12ljcT8bME"
 
-    header={
-        "Authorization": "Bearer {}".format(token)
-    }
-    
-    payload  = {
-        "channel" : "C03EW561RJB"
-    }
-    
-    res = requests.get(url, headers=header, params=payload)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            # 'slack': res.json()
-            res
-        })
-    }
+if __name__ == "__main__":
+    res = lambda_handler(None, None)
+    print(res)
